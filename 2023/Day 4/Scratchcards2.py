c9a4ac8232386e4ca88c97621f7ed173466d7df0
Saomya Tangri <44325696+saomyatangri@users.noreply.github.com>
2023-12-04 12:03:28 -0500

    day 4 part 2

---
+++ 2023/Day 4/Scratchcards2.py
@@ -0,0 +1,26 @@
+import fileinput
+import numpy as np
+
+def scratchcards(input_file):
+    frequencies = np.ones(206)
+    frequencies[0] = 0
+    card_num = 0
+    
+    for line in fileinput.input(files = input_file):
+        card_num += 1
+        pair = line.split("|")
+        winning_numbers = np.array(pair[0].split())
+        our_numbers = np.array(pair[1].split())
+        
+        winning_numbers = winning_numbers[2:]
+        our_winning_nums = np.intersect1d(winning_numbers, our_numbers)
+        matching_nums = our_winning_nums.size
+
+        if matching_nums != 0:
+            frequencies[card_num+1:card_num+matching_nums+1] += 1*frequencies[card_num]
+    
+    return int(frequencies.sum())
+
+
+if __name__ == "__main__":
+    print(scratchcards('actual_input.txt'))
